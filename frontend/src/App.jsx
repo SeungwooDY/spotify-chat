cdbd2e45a38aa120a097cb31d6ad3f682f023e22
Emily Middleton <emiddleton@smith.edu>
2026-05-25 15:38:04 -0500

    feat: router functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+      {/* Page Content */}
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
