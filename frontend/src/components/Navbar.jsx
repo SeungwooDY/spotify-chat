ea7dab31b4683a5e15acb5c61b56a50e511d04fd
Emily Middleton <emiddleton@smith.edu>
2026-05-25 16:02:29 -0500

    feat: Navbar functionality

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import "../App.css";
+import {NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="nav-text">
+          <NavLink to="/profile" className="nav-link">Profile</NavLink>
+          <NavLink to="/" className="nav-link">Discover</NavLink>
+          <NavLink to="/liked-songs" className="nav-link">Liked Songs</NavLink>
+          <NavLink to="/top-artists" className="nav-link">Top Artists</NavLink>
+          <NavLink to="/forum" className="nav-link">Forum</NavLink>
+          <NavLink to="/top-songs" className="nav-link">Top Songs</NavLink>
+          <NavLink to="/inbox" className="nav-link">Inbox</NavLink>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Navbar;
