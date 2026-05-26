5eae9448a3125770a89370e7b6eea5ae2f0546c0
Emily Middleton <emiddleton@smith.edu>
2026-05-26 09:03:28 -0500

    feat: initial ui

--- frontend/src/pages/ForumPage.jsx
+++ frontend/src/pages/ForumPage.jsx
@@ -1,7 +1,38 @@
+import "../styling/Forum.css";
+import { useState } from 'react';
+import ForumPost from "../components/ForumPost";
+
 const ForumPage = () => {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <>
-      Forum Page 
+      <section className="page-content">
+        <h1 className="page-title">Forum Page</h1>
+
+        <section className="search-container">
+          <input 
+          className="search-bar"
+          type="text" 
+          placeholder="search by name..."
+          aria-label="post search bar"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          >
+          </input>
+
+          <button className="create-button" aria-label="create post" label="create post">create post</button>
+        </section>
+        
+        <section className="post-container">
+          <ForumPost postTitle={"title"}/>
+          <ForumPost postTitle={"title"}/>
+          <ForumPost postTitle={"title"}/>
+          <ForumPost postTitle={"title"}/>
+          <ForumPost postTitle={"title"}/>
+        </section>
+
+      </section>
     </>
   );
 }
