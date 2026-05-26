cdab170984477afbe35811b8c52239d222153b57
Emily Middleton <emiddleton@smith.edu>
2026-05-25 20:34:46 -0500

    feat: ui initial layout

--- frontend/src/pages/LikedSongsPage.jsx
+++ frontend/src/pages/LikedSongsPage.jsx
@@ -1,7 +1,36 @@
+import "../styling/LikedSongs.css";
+
 const LikedSongsPage = () => {
   return (
     <>
-      Liked Songs Page
+      <section className="page-content">
+        <div className="page-title">Liked Songs</div>
+
+        <div className="song-container">
+          <div className="song-row-container">
+            <h3>Genre: xxx</h3>
+            <div className="song-row">
+              <div className="song-card"> Song </div>
+              <div className="song-card"> Song </div>
+              <div className="song-card"> Song </div>
+            </div>
+          </div>
+
+          <div className="song-row-container">
+            <h3>Genre: xxx</h3>
+            <div className="song-row">
+
+            </div>
+          </div>
+
+          <div className="song-row-container">
+            <h3>Genre: xxx</h3>
+            <div className="song-row">
+
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
