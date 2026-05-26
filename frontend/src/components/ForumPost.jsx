61204dc14c363dcb0d00e1b31c1fe619270bef71
Emily Middleton <emiddleton@smith.edu>
2026-05-26 09:03:20 -0500

    refactor: separate forum post component

---
+++ frontend/src/components/ForumPost.jsx
@@ -0,0 +1,13 @@
+import "../styling/Forum.css";
+
+const ForumPost = ( {postTitle} ) => {
+  return (
+    <>
+      <div className="post">
+        <section className="post-header">{postTitle}</section>
+        
+      </div>
+    </>
+  );
+}
+export default ForumPost;
