44d0a2a21af4ee25862d2c7fe0fb92c7f967484d
Emily Middleton <emiddleton@smith.edu>
2026-05-26 09:32:35 -0500

    feat: initial create post popup

---
+++ frontend/src/components/CreatePost.jsx
@@ -0,0 +1,14 @@
+import "../styling/Forum.css";
+
+const CreatePost = () => {
+  return (
+    <>
+      <section className="form-overlay">
+        <div className="new-post-form">
+
+        </div>
+      </section>
+    </>
+  );
+}
+export default CreatePost;
